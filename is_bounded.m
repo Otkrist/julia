ce4910fc05914dab900efb744000ffab7373fd0f
Otkrist Gupta <otkrist@mit.edu>
2017-03-24 11:30:48 -0400

    adding color

--- is_bounded.m
+++ is_bounded.m
@@ -2,16 +2,17 @@ function [tval] = is_bounded( z,c )
 %IS_BOUNDED Summary of this function goes here
 %   Detailed explanation goes here
 
-N_ITER = 500;
+N_ITER = 50000;
 
 %% Inefficient iterant.
 for i=1:N_ITER
     z = z*z + c;
     if abs(z) > 4
-        tval = 0;
+        tval = 1.0/i;0.5 * (1 - 1.0/i);
         return
     end
 end
+
 tval = 1;
 end
 
--- simple_draw.m
+++ simple_draw.m
@@ -1,9 +1,10 @@
+clc
 clear all
 close all
 
 %% Globals.
 UB = 2.0;
-p = 0.3 + 0.5i;
+p  = 0.484;
 
 %% Canvas size:
 M = 1920;
@@ -11,14 +12,22 @@ N = 1080;
 get_cplx = @(r,c)((UB * (2 * r-M))/M + (UB * (2 * c-N))/N * 1i);
 
 %%
-I = zeros(M,N,3);
+I = zeros(M,N);
 
 for r=1:M
   for c=1:N
     z = get_cplx(r,c);
-    I(r,c,:) = is_bounded(z,p);
+    I(r,c) = is_bounded(z,p);
   end
 end
 
 %%
-image(I);
+
+II = zeros(M,N,3);
+for r=1:M
+  for c=1:N
+    II(r,c,:) = (I(r,c))  * [0.5 1 0.83];
+  end
+end
+
+image(II);
