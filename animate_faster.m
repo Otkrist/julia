439983fa05e47fb098a2633cc26597f061c75dea
Otkrist Gupta <otkrist@mit.edu>
2017-03-24 15:06:07 -0400

    Faster animation

--- animate_faster.m
+++ animate_faster.m
@@ -29,18 +29,53 @@ figure;
 set(gcf, 'Position', get(0,'Screensize')); % Maximize figure.
 
 I = ones(M,N,3);
+A = [];
+ctr = M*N;
 
 for jj=1:N_ITER
-  for r=1:M
-    for c=1:N
-      if I(r,c,2) == 1
+  if ctr > M*N*0.1
+    ctr = 0;
+    for r=1:M
+      for c=1:N
+        if I(r,c,2) == 1
           z = C(r,c);
           [tval z] = is_bounded(z,p,1,jj);
           I(r,c,:) = tval* [0.05 1 1];
           C(r,c) = z;
+          ctr = ctr + 1;
+        end
       end
     end
+  else
+    if length(A) == 0 || mod(jj, 10) == 0
+      l = 0;
+      r_c = I(:,:,2) == 1;
+      ctr = sum(r_c(:));
+      if ctr < 10
+          break
+      end
+      A = uint16(zeros(ctr,2));
+      for r=1:M
+          for c=1:N
+            if (I(r,c,2) == 1)
+                l = l + 1;
+                A(l,:) = [r c];
+            end
+          end 
+      end
+    end
+    for l=1:size(A,1)
+        r = A(l,1);
+        c = A(l,2);
+        if I(r,c,2) == 1
+          z = C(r,c);
+          [tval z] = is_bounded(z,p,1,jj);
+          I(r,c,:) = tval* [0.05 1 1];
+          C(r,c) = z;
+        end
+    end
   end
+
   if mod(jj,1) == 0
       imagesc(I);
       title(sprintf('Iteration = [%d]', jj));
